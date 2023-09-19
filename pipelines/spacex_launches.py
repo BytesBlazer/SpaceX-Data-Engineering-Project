02757b7fc95957ed55a65301fa75bae352603fd5
BytesBlazer <veer28rohit@gmail.com>
2023-09-19 13:08:25 +0530

    Documented lanches DAG

--- pipelines/spacex_launches.py
+++ pipelines/spacex_launches.py
@@ -1,3 +1,4 @@
+# Import necessary libraries and modules
 import os
 import json
 import requests
@@ -10,27 +11,16 @@ from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
 from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 
-# set the job interval 
+# Define constants and environment variables
 SCHEDULE_INTERVAL = "@once"
-
-# staging path 
 SPACEX_STAGING = os.environ.get("SPACEX_STAGING")
-
-# project home directory
 SPACEX_HOME = os.environ.get("SPACEX_HOME")
-
-# airflow home directory
 AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME")
-
-# Mongodb connection path
 MONGO_CONNECTION = "mongodb://localhost:27017"
-
-# json and csv file location
 JSON_FILE_LOCATION = f"{SPACEX_STAGING}/json/spacex_launches.json"
 CSV_FILE_LOCATION = f"{SPACEX_STAGING}/csv/spacex_launches_transformed.csv"
 
-
-# fetch launches data from spacex api and store into json file
+# Task 1: Extract data from SpaceX API
 def extract():
     url = "https://api.spacexdata.com/v4/launches"
     response = requests.get(url)
@@ -39,7 +29,7 @@ def extract():
         with open(JSON_FILE_LOCATION, "w") as file:
             json.dump(response.json(), file)
 
-# load fetched data into mongodb spacex_lake database and launches collection
+# Task 2: Load extracted data into MongoDB
 def load_mongo():
     client = MongoClient(MONGO_CONNECTION)
     db = client["spacex_lake"]
@@ -52,7 +42,7 @@ def load_mongo():
     collection.insert_many(records)
     client.close()
 
-# transform json data and store it into csv file
+# Task 3: Transform data into CSV format
 def transform():
     fields = ['id','rocket','success','launchpad','flight_number','name','date_utc','upcoming','links']
     launches = pd.read_json(JSON_FILE_LOCATION)
@@ -65,7 +55,7 @@ def transform():
     launches.rename({"links": "webcast"}, axis=1)
     launches.to_csv(CSV_FILE_LOCATION)
 
-# define default arguments for dag
+# Define default arguments for the DAG
 default_args = {
     "owner" : "airflow",
     "start_date" : dt.datetime(2023, 9, 6),
@@ -75,37 +65,37 @@ default_args = {
     "retry_delay" : dt.timedelta(minutes=1)
 }
 
-# define the dag
+# Create the DAG
 dag = DAG(
     dag_id = "spacex_launches_pipeline",
-    description = "This dag extracts launches data from api and loads into mongodb launches collection",
+    description = "This DAG extracts launches data from the API and loads it into MongoDB launches collection",
     schedule_interval = SCHEDULE_INTERVAL,
     template_searchpath = [f"{AIRFLOW_HOME}/dags/"],
     default_args = default_args
 )
 
-# define extract task with python operator
+# Task 1: Extract data from SpaceX API
 extract_task = PythonOperator(
     task_id = "spacex_launches_extract",
     python_callable = extract,
     dag = dag
 )
 
-# define transform task
+# Task 2: Transform data into CSV format
 transform_task = PythonOperator(
     task_id = "spacex_launches_transform_to_csv",
     python_callable = transform,
     dag = dag
 )
 
-# define load mongodb task 
+# Task 3: Load data into MongoDB
 load_mongo_task = PythonOperator(
     task_id = "spacex_launches_load_to_mongo",
     python_callable = load_mongo,
     dag = dag
 )
 
-# define load postgres task
+# Task 4: Load data into PostgreSQL
 load_postgres_task = PostgresOperator(
     task_id="spacex_launches_csv_to_postgres",
     postgres_conn_id="spacex_postgres",
@@ -113,7 +103,7 @@ load_postgres_task = PostgresOperator(
     dag=dag
 )
 
-# define task to load into Google Cloud Storage
+# Task 5: Load CSV data into Google Cloud Storage (GCS)
 load_gcs_task = LocalFilesystemToGCSOperator(
     task_id = 'spacex_launches_csv_gcs',
     src = CSV_FILE_LOCATION,
@@ -123,9 +113,9 @@ load_gcs_task = LocalFilesystemToGCSOperator(
     dag = dag
 )
 
-# define task to load BigQuery from GCS
+# Task 6: Load data from GCS into BigQuery
 load_bigquery_task = GCSToBigQueryOperator(
-    task_id = "spcex_launches_gcs_bigquery",
+    task_id = "spacex_launches_gcs_bigquery",
     bucket="spacex_data_lake",
     source_objects=["spacex_launches_transformed.csv"],
     destination_project_dataset_table="SpaceX_dataset.Launches",
@@ -134,7 +124,7 @@ load_bigquery_task = GCSToBigQueryOperator(
     dag=dag
 )
 
-# define the task dependencies
+# Define task dependencies
 extract_task >> transform_task
 transform_task >> [load_mongo_task, load_postgres_task, load_gcs_task]
 load_gcs_task >> load_bigquery_task
